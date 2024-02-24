b50dd3143a8d4ae2b97187091762348990b3b15b
dhanasekaran3004 <83817146+dhanasekaran3004@users.noreply.github.com>
2024-02-24 23:03:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/input">
+        <Route>
           <Route path='/input' index element={<Home />} />
           <Route path="/input/success" element={<Success />} />
           <Route path="*" element={<Home />} />
